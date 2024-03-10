76b4c7f024e5540e0cf89c4ea63b97e14fcee76c
AlphaN00b <karimdope1@gmail.com>
2024-03-10 12:39:54 +0100

    implemented all the exercise requirements

--- index.js
+++ index.js
@@ -1,9 +1,23 @@
 const grid = document.querySelector("#Container");
 const square = document.createElement("div");
+const newGrid = document.querySelector("#newGrid");
 
 generateGrid(16)
 
+newGrid.onclick = function() {
+    let squaresNum = Number(window.prompt("Enter a number between 1-100:"));
+    grid.innerHTML = "";
+    if (squaresNum > 100 || squaresNum <= 0) {
+        window.alert("your number is outside the allowed range");
+        generateGrid(16)
+    }
+    else {
+        generateGrid(squaresNum)
+    }
+}
+
 function generateGrid(dimensions) {
+    
     let collumns = 0;
     while (collumns < dimensions) {
         let squareNumber = 0;
@@ -14,6 +28,8 @@ function generateGrid(dimensions) {
             square.onmouseenter = function() {
                 square.classList.add("black");
             }
+            square.style.width = `${512/dimensions}px`;
+            square.style.height = `${512/dimensions}px`;   
             squareNumber ++;
         }
         collumns ++;    
